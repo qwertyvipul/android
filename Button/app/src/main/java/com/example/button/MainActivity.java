39686996c41793ffcc750fc3f065b6a061d35eea
Vipul Sharma <vipuls526@gmail.com>
2018-10-26 15:58:20 +0530

    Button App

---
+++ Button/app/src/main/java/com/example/button/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.button;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnClickHereMethod(View view){
+        Log.i("info", "Button was Clicked!");
+    }
+}
