1f44ad8d362fdadc35d94359bf5c67e014e761cc
Vipul Sharma <vipuls526@gmail.com>
2018-10-31 16:30:20 +0530

    Alert Dialog

---
+++ AlertDialog/app/src/main/java/com/example/alertdialog/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.alertdialog;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new AlertDialog.Builder(this)
+                .setIcon(R.drawable.ic_launcher_background)
+                .setTitle("This is an alert")
+                .setMessage("Click the button!")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "Yes Button Clicked!", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
+    }
+}
